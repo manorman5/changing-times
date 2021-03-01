fe34aeba8060cb31fc5a61862966909a86d83b59
GIT SERVICE NAME (NOT USERNAME) <42501578+manorman5@users.noreply.github.com>
2021-03-01 13:36:10 -0700

    adding updates to age tab

--- tabs/age.py
+++ tabs/age.py
@@ -108,6 +108,8 @@ class Layout(layout.Layout):
                     yrange=[-2*60,2*60], 
                     ytitle='Demeaned Mean Finish Time (min)',
                     yvar="meantime_demeaned"
+                    hovertext=True,
+                    marker_opacity=0.25
                 )
             ]
         )
